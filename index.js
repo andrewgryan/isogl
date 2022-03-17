8de131aa7193811721bfc896be988df6bb3c1ab6
andrewgryan <andrew.ryan@metoffice.gov.uk>
2022-03-17 10:19:36 +0000

    convert code to use texture

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ export function Quad() {
     this.source = {
         vert: `#version 300 es
 in vec2 a_position;
-in vec4 a_color;
+in vec2 a_texcoord;
 
-out vec4 v_color;
+out vec2 v_texcoord;
 
 void main() {
-    v_color = a_color;
+    v_texcoord = a_texcoord;
     gl_Position = vec4(a_position, 0.0, 1.0);
 }
 `,
         frag: `#version 300 es
 precision highp float;
-in vec4 v_color;
+
+in vec2 v_texcoord;
+uniform sampler2D u_texture;
 out vec4 outColor;
 
 void main() {
-    outColor = v_color;
+    outColor = texture(u_texture, v_texcoord);
 }
 `,
     };
@@ -53,30 +55,49 @@ Quad.prototype.mount = function (gl) {
     gl.enableVertexAttribArray(positionAttributeLocation);
     gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-    // Buffer for a_color
-    const colorAttributeLocation = gl.getAttribLocation(
+    // Buffer for a_texcoord
+    const texcoordAttributeLocation = gl.getAttribLocation(
         this.program,
-        "a_color"
+        "a_texcoord"
     );
 
-    // Bind ARRAY_BUFFER to color location
-    gl.enableVertexAttribArray(colorAttributeLocation);
+    // Bind ARRAY_BUFFER to texcoord location
+    gl.enableVertexAttribArray(texcoordAttributeLocation);
 
     // Fill buffer with data
-    const rgbas = [
-        1, 1, 0, 1,
-        0, 1, 0, 1,
-        1, 0, 1, 1,
-        1, 0, 1, 1,
-        0, 1, 1, 1,
-        1, 1, 0, 1,
-    ];
-    const colorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(rgbas), gl.STATIC_DRAW);
+    const uvs = [0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0];
+    const texcoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, texcoordBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(uvs), gl.STATIC_DRAW);
 
-    // Tell WebGL how to pull a_color out of buffer
-    gl.vertexAttribPointer(colorAttributeLocation, 4, gl.FLOAT, false, 0, 0);
+    // Tell WebGL how to pull a_texcoord out of buffer
+    gl.vertexAttribPointer(texcoordAttributeLocation, 2, gl.FLOAT, false, 0, 0);
+
+    // Create texture and fill it with fake data
+    const px = 2;
+    const py = 2;
+    const pixels = new Uint8Array([
+        0, 0, 255, 255, 0, 255, 0, 255, 255, 0, 0, 255, 255, 0, 255, 255,
+    ]);
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.RGBA,
+        px,
+        py,
+        0,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        pixels
+    );
+    gl.generateMipmap(gl.TEXTURE_2D)
+
+    // Configure repeat and interpolation of texture
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
 };
 
 Quad.prototype.render = function (gl) {
